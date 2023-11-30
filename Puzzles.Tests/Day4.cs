ae2952b7d2a9fbd39b29598ae8bdc535a6e8822e
Dylan Munyard <dylanmunyard@sixpivot.com.au>
2023-11-30 20:54:45 +1000

    Day 4 using ChatGPT to create an overlap function.

---
+++ Puzzles.Tests/Day4.cs
@@ -0,0 +1,33 @@
+using aspire_aoc.Puzzles.Day4;
+using FluentAssertions;
+
+namespace Puzzles.Tests;
+
+public class Day4Tests
+{
+    private readonly Day4 _puzzle = new();
+
+    [Theory]
+    [InlineData(true, 2)]
+    [InlineData(false, 580)]
+    public async Task Part1(bool solveSample, int answer)
+    {
+        // Act
+        var solution = await _puzzle.SolvePart1(solveSample);
+        
+        // Assert
+        solution.Should().Be(answer);
+    }
+    
+    [Theory]
+    [InlineData(true, 4)]
+    [InlineData(false, 2752)]
+    public async Task Part2(bool solveSample, int answer)
+    {
+        // Act
+        var solution = await _puzzle.SolvePart2(solveSample);
+        
+        // Assert
+        solution.Should().Be(answer);
+    }
+}
